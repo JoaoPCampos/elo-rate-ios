56b339a4c81b42ed3a0ca36cdba3c8011d84bd13
João Campos <jonybfc@gmail.com>
2018-08-14 16:55:02 +0100

    First commit

---
+++ Elo Rate/Elo Rate/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Elo Rate
+//
+//  Created by João Campos on 14/08/2018.
+//  Copyright © 2018 João Campos. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+}
+
